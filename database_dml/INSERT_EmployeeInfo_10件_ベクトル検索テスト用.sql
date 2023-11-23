ed5e453659e29e56f1c6f235e79bceb62085f086
yazaki-tatsuya <yazaki.tatsuya.0813@gmail.com>
2023-11-23 16:30:17 +0900

    ベクトル検索用のデータINSERT文の追加

---
+++ database_dml/INSERT_EmployeeInfo_10件_ベクトル検索テスト用.sql
@@ -0,0 +1,12 @@
+INSERT INTO HumanResourceMaster (employee_no, first_name, last_name, email_address, joining_date, retirement_date, department_code)
+VALUES
+('00010001', 'Takashi', 'Tanaka', 't.tanaka@email.com', '2015-04-01', NULL, 'FIN01'),
+('00010002', 'Akira', 'Sato', 'a.sato@email.com', '2016-07-15', NULL, 'TEC02'),
+('00010003', 'Yuko', 'Kobayashi', 'y.kobayashi@email.com', '2018-01-10', NULL, 'HEA03'),
+('00010004', 'Ken', 'Yamamoto', 'k.yamamoto@email.com', '2017-09-05', NULL, 'ENE04'),
+('00010005', 'Mai', 'Nakamura', 'm.nakamura@email.com', '2019-05-20', NULL, 'MAR05'),
+('00010006', 'Hiroshi', 'Kato', 'h.kato@email.com', '2014-03-01', NULL, 'REA06'),
+('00010007', 'Ayumi', 'Ito', 'a.ito@email.com', '2020-11-01', NULL, 'EDU07'),
+('00010008', 'Shota', 'Watanabe', 's.watanabe@email.com', '2017-04-17', NULL, 'AUT08'),
+('00010009', 'Nao', 'Suzuki', 'n.suzuki@email.com', '2018-08-23', NULL, 'RET09'),
+('00010010', 'Eri', 'Yamashita', 'e.yamashita@email.com', '2016-02-15', NULL, 'PHA10');
